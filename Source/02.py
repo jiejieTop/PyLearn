bd873b534b181885cb6283c82251668d521476b5
jiejieTop <1161959934@qq.com>
2018-11-23 23:26:21 +0800

    03.py

---
+++ Source/02.py
@@ -0,0 +1,8 @@
+database = [['jiejie','1234'],['jiejieTop','5678']]
+usermane = input('user mane :')
+passward = input('passward :')
+if[usermane,passward] in database:
+    print('ok!')
+else:
+    print('Not Ok!')
+
---
+++ Source/03.py
@@ -0,0 +1,10 @@
+lst = [1,2,3,4]
+lst.append(5)
+print(lst)
+lst.clear()
+print(lst)
+
+
+a = [1]
+b = [2]
+
